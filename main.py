b78d796b0dd098ba190a1b15c6fdd43ae9d0f898
ShadyBarrios <scottpsn16@gmail.com>
2025-04-07 13:43:47 -0700

    Commented timer

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def parallelize(combos, partitions, processes):
 if __name__ == "__main__":
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     processes = multiprocessing.cpu_count()
-    start = time.time()
+    # start = time.time()
 
     emptyCombo = ['']
     oneCombos = alphabet.copy()
@@ -84,8 +84,8 @@ if __name__ == "__main__":
     fourResults = parallelize(oneCombos, threePartitions, processes)
     check(fourResults)
 
-    print(time.time() - start)
-    exit(0)
+    # print(time.time() - start)
+    # exit(0)
 
     print("Testing 5 character passwords")
     fiveResults = parallelize(twoCombos, threePartitions, processes)
